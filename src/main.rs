12acf99b368a5df74dd4419f64ae710830cdf66a
James John McGuire <jamesjohnmcguire@gmail.com>
2025-01-02 14:33:26 +0900

    Continuations

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // use std::{thread, time};
 
 use ferris_says::say;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 use std::io::{stdout, BufWriter};
 
@@ -18,11 +20,28 @@ fn main()
 
     println!("Guess the number!");
 
+    let mut generator = rand::thread_rng();
+
+    let secret_number = generator.gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let instance = io::stdin();
+
+    let result = instance.read_line(&mut guess);
+
+    result.expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
