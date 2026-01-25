16ba489ea622e105d74401bde98b55389045fdaa
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-26 00:03:22 +0900

    Fill Out Environment Checking

--- inboxes/src/lib.rs
+++ inboxes/src/lib.rs
@@ -1,7 +1,31 @@
 use rand;
+use std::env;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+pub fn inboxes()
+{
+    let mut key = "HOME";
+
+    if (env::consts::OS == "windows")
+    {
+        key = "USERPROFILE";
+    }
+    else
+    {
+        println!("This OS is not supported yet.");
+    }
+
+    let user_profile = match env::var(key)
+    {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Couldn't read {key}: {e}");
+            return;
+        }
+    };
 }
 
 pub fn inbox(directory: &str) -> bool
