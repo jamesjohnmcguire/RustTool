83cc395250b75b43b6952d3172014c86ba872d7d
James John McGuire <jamesjohnmcguire@gmail.com>
2026-02-03 17:00:49 +0900

    Add Minor Refinenments

--- inboxes/src/lib.rs
+++ inboxes/src/lib.rs
@@ -1,9 +1,7 @@
-use rand;
+use anyhow::Result;
+use fs_extra::dir::{move_dir, CopyOptions};
 use std::env;
-
-use fs_extra::dir::{copy, CopyOptions};
 use std::path::Path;
-use std::io::Result;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -14,33 +12,21 @@ pub fn inboxes() -> Result<()>
     let src = "source_dir";
     let dst = "destination_dir";
 
-    let mut key = "HOME";
-
-    if (env::consts::OS == "windows")
-    {
-        key = "USERPROFILE";
-    }
-    else
-    {
-        println!("This OS is not supported yet.");
-    }
-
-    let user_profile = match env::var(key)
-    {
-        Ok(val) => val,
-        Err(e) => {
-            println!("Couldn't read {key}: {e}");
-            return Err(
-                std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Failed to get user profile"));
-        }
+    let key = if cfg!(windows) {
+        "USERPROFILE"
+    } else if cfg!(unix) {
+        "HOME"
+    } else {
+        anyhow::bail!("This OS is not supported yet");
     };
+
+    let user_profile = env::var(key)?;
 
     // Default options: won't overwrite existing files
     let options = CopyOptions::new();
+    let options = options.content_only(true); 
 
-    copy(src, dst, &options);
+    move_dir(src, dst, &options)?;
     Ok(())
 }
 
